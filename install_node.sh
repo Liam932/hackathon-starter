d10d53d232060a63c953bab99b317511ac27295b
Liam Childs <liam.childs91@gmail.com>
2016-11-22 19:35:39 +0000

    wip

--- install_node.sh
+++ install_node.sh
@@ -3,4 +3,5 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_DIR="/var/jenkins_home/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install 6
